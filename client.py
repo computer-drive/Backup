3c81ef75b7baf46d1ed7b9219f5504c100026c02
Wangyaqi <ccc123zhy@outlook.com>
2026-05-02 15:46:31 +0800

    重构客户端代码，添加输入处理线程，优化心跳包发送逻辑，增强连接稳定性

--- client.py
+++ client.py
@@ -1,27 +1,105 @@
 import socket
 from libs.const import *
 from libs.protocol import Protocol
+import time
+import threading
+import queue
+
+
+def input_worker(conn: socket):
+    while True:
+        line = input(">> ")
+        if line == "exit":
+            break
+        else:
+            try:
+                conn.send(
+                    Protocol(0x5F86C001, PROTOCOL_COMMAND_TEST, PROTOCOL_PAYLOAD_STRING, 0, line.encode() ).
+                    encode()
+                )
+                
+            except Exception as e:
+                import traceback
+                traceback.print_exc()
+
+            print("send command.")
+                
+
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 8080))
 
-print("连接成功")
+print("connecting...")
+try:
+    client.connect(("127.0.0.1", 8080))
+except Exception as e:
+    print(e)
+    print("connect failed.")
+    exit(1)
 
-def send_packet(string: str):
-    payload = string.encode()
+print("connect success.")
 
-    packet = Protocol(
-        magic=0x5F86C001,
-        command=0x0001,
-        payload_type=0x03,
-        payload=payload,
-    )
+client.send(Protocol(0x5F86C001, PROTOCOL_COMMAND_HEARTBEAT, PROTOCOL_PAYLOAD_BINARY).encode())
 
-    client.send(packet.encode())
-    print("发送成功") 
+q = queue.Queue()
+input_worker_thread = threading.Thread(target=input_worker, args=(client,), daemon=True)
+input_worker_thread.start()
 
+last_heartbeat = time.time()
+
+client.settimeout(5)
+
+def send_heartbeat():
+    global last_heartbeat
+    if last_heartbeat + 5 < time.time():
+        client.send(Protocol(0x5F86C001, PROTOCOL_COMMAND_HEARTBEAT, PROTOCOL_PAYLOAD_BINARY).encode())
+        last_heartbeat = time.time()
+        print("sned heartbeat.")
 
 while True:
-    send_packet(input())
+    try:
+        send_heartbeat()
+
+        try:
+            header = b''
+            while len(header) < 12:
+                chunk = client.recv(12 - len(header))
+                if not chunk:
+                    break
+                header += chunk
+        except socket.timeout:
+            continue
+        
+        if not header:
+            continue    
+        
+        protocol = Protocol()
+        payload_length = protocol.decode_header(header)
+
+        if protocol.command == PROTOCOL_COMMAND_DISCONNECT:
+            print("server disconnect.")
+            break
+
+        if protocol.command == PROTOCOL_COMMAND_SERVER_CLOSED:
+            print("server closed.")
+            break
+
+        print(protocol)
+
+        
+        
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        break
+
+client.close()
+
+input_worker_thread.join()
+
+print("disconeected.")
+
+            
+    
 
 
